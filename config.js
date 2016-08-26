c55807016488800b5e55df97d25be34c2a349da6
Martin Wimpress <code@flexion.org>
2016-08-26 11:04:17 +0100

    Initial commit of v0.3

---
+++ config.js
@@ -0,0 +1,20 @@
+// the name of your app as used in the click package (required)
+var webappName = "lastpass-mobile.flexiondotorg"
+
+// the start url of your app (required)
+var webappUrl = "https://lastpass.com/mobile/"
+
+// the the pattern that defines which links are considered
+// local and which are opened in an external browser (required)
+var webappUrlPattern = "https?://lastpass.com/*"
+
+// a user agent override (optional)
+// var webappUA = "uncomment this line and set your User Agent string here between these quotes, if you need to override it"
+
+// Haptic feedback for links (note, this does not work if your site
+// uses javascript functions to open links) (optional)
+// var hapticLinks = "true"
+
+// Audible feedback when clicking links (the same constraints as
+// for hapticLinks apply) (optional)
+// var audibleLinks = "true"
